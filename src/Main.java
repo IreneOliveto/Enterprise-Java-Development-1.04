a36757c043123c0692daaee73904aa4690b06374
IreneOliveto <nene.ol@hotmail.it>
2022-07-16 11:04:45 +0200

    1stCommit_Exercise1_Week1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numbers[] = new int[]{1, 2, 3, 4, 5};
+
+        int smallest = numbers[0];
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest)
+                largest = numbers[i];
+            else if (numbers[i] < smallest)
+                smallest = numbers[i];
+        }
+
+        System.out.println(+(largest-smallest));
+    }
+}
